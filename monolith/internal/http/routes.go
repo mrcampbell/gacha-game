2fb851993a42b2fdd5b16c2c067cd06df445bd6a
Mike Campbell <mike.campbell@getweave.com>
2021-01-05 16:00:34 -0700

    got monolith wired up

---
+++ monolith/internal/http/routes.go
@@ -0,0 +1,15 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mrcampbell/gacha-game/monolith/internal/http/handlers"
+)
+
+func RegisterHandlers(r *gin.Engine) {
+	registerSwaggerHandlers(r)
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/ping", handlers.Ping)
+		// v1.POST("/my/team", handlers.MyTeam)
+	}
+}
---
+++ monolith/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mrcampbell/gacha-game/monolith/internal/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	http.RegisterHandlers(r)
+
+	if err := r.Run(); err != nil { // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+		println(err)
+	}
+}
